904d2afab91ebbccb2375705c9f68a9c5f182dd7
Myrmex <dfusi@hotmail.com>
2024-05-08 15:58:31 +0200

    Proxy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { EnvServiceProvider } from '@myrmidon/ng-tools';
 import { NgeMonacoModule } from '@cisstech/nge/monaco';
+
+import { EnvServiceProvider } from '@myrmidon/ng-tools';
+import {
+  PROXY_INTERCEPTOR_OPTIONS,
+  ProxyInterceptor,
+} from '@myrmidon/cadmus-refs-lookup';
 
 import { routes } from './app.routes';
 
@@ -17,5 +22,17 @@ export const appConfig: ApplicationConfig = {
     EnvServiceProvider,
     importProvidersFrom(HttpClientModule),
     importProvidersFrom(NgeMonacoModule.forRoot({})),
+    // proxy
+    { provide: HTTP_INTERCEPTORS, useClass: ProxyInterceptor, multi: true },
+    {
+      provide: PROXY_INTERCEPTOR_OPTIONS,
+      useValue: {
+        proxyUrl: (window as any).__env?.proxyUrl as string,
+        urls: [
+          'http://lookup.dbpedia.org/api/search',
+          'http://lookup.dbpedia.org/api/prefix',
+        ],
+      },
+    },
   ],
 };
